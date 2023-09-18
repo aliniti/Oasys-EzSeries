d00cc47653a5e74993c58e2a2ae9454f8aa057a1
Kurisu <robin@kurisumaki.se>
2023-09-18 14:34:53 -0700

    Nidalee: heal fix

--- EzSeries/Champions/Nidalee/Menu.cs
+++ EzSeries/Champions/Nidalee/Menu.cs
@@ -20,21 +20,13 @@ namespace EzSeries.Champions.Nidalee
             _main.AddItem(new Switch("Auto change forms", true));
             _main.AddItem(new InfoDisplay { Information = "Auto heal allies" });
             
-            var allies = UnitManager.AllyChampions.OrderBy(a => a.UnitComponentInfo.SkinName);
+            var allies = UnitManager.AllyChampions.OrderBy(a => a.ModelName);
             foreach (var a in allies)
             {
-                _main.AddItem(new Switch("Auto heal " + a.UnitComponentInfo.SkinName, true));
-                _main.AddItem(new Counter(a.UnitComponentInfo.SkinName + " heal pct", 75, 1, 100));
+                _main.AddItem(new Switch("Auto heal " + a.ModelName, true));
+                _main.AddItem(new Counter(a.ModelName + " heal pct", 75, 1, 100));
             }
             _config.AddGroup(_main);
-
-            _clear = new Group("Clearing");
-            _clear.AddItem(new Switch("Require min auto attacks", true));
-            _clear.AddItem(new Counter("Min auto attacks", 2, 1, 5));
-            _clear.AddItem(new Counter("Min auto attacks w/ heal", 4, 1, 5));
-            _clear.AddItem(new Counter("Fast clear at/after lvl", 11, 1, 17));
-            //_clear.AddItem(new Switch("Experimental wave clear", false) );
-            _config.AddGroup(_clear);
             
             _draw = new Group("Drawings");
             // _draw.AddItem(new Switch("Draw Javelin Toss range", true));
@@ -46,6 +38,15 @@ namespace EzSeries.Champions.Nidalee
             // _misc.AddItem(new Switch("Auto Bushwhack Immobile", true));
             _misc.AddItem(new Switch("Debug Cooldowns", false));
             _config.AddGroup(_misc);
+            
+            _clear = new Group("Clearing: Advanced");
+            _clear.AddItem(new InfoDisplay { Title = "Don't change these if you don't know what your doing." });
+            _clear.AddItem(new Switch("Require min auto attacks", true));
+            _clear.AddItem(new Counter("Min auto attacks", 2, 1, 5));
+            _clear.AddItem(new Counter("Min auto attacks w/ heal", 4, 1, 5));
+            _clear.AddItem(new Counter("Fast clear at/after lvl", 11, 1, 17));
+            //_clear.AddItem(new Switch("Experimental wave clear", false) );
+            _config.AddGroup(_clear);
         }
 
         public static bool Debug()
@@ -60,12 +61,12 @@ namespace EzSeries.Champions.Nidalee
 
         public static bool AutoHealAlly(AIBaseClient unit)
         {
-            return _main.GetItem<Switch>("Auto heal " + unit.UnitComponentInfo.SkinName).IsOn;
+            return _main.GetItem<Switch>("Auto heal " + unit.ModelName).IsOn;
         }
         
         public static int AutoHealAllyPct(AIBaseClient unit)
         {
-            return _main.GetItem<Counter>(unit.UnitComponentInfo.SkinName + " heal pct").Value;
+            return _main.GetItem<Counter>(unit.ModelName + " heal pct").Value;
         }
 
         public static bool SafeToFastClear()
